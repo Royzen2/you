0e26c639e0e850515bfe0410ee3226a37ead9f46
Royzen2 <64889727+Royzen2@users.noreply.github.com>
2021-12-09 18:31:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 
 var veh_n = "";
 var veh_price = 0;
+var veh_i = 0;
 //смени на свое
 new client  = CefClientApiMessage(); //это тебе не надо ( мой скрипт )
-client.event.subscribe("data:param", (veh_name, price) => {
+client.event.subscribe("data:param", (veh_name, veh_id, price) => {
       veh_n = veh_name;
       veh_price = price; 
+      veh_i = veh_id;
 });
 
 //тут событие когда он нажал на кнопку, передаем в паблик
 
 //на свое
-client.event.send("btn:buy_veh", veh_n, price); 
+client.event.send("btn:buy_veh", veh_n, price, veh_id); 
